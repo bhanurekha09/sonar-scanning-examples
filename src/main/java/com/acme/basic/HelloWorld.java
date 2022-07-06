8fece9ecd02fd1ae956db09e026b19d395c348b4
bhanurekha09 <71223482+bhanurekha09@users.noreply.github.com>
2022-07-06 22:46:47 +0530

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -3,11 +3,11 @@ package com.acme.basic;
 public class HelloWorld {
 
   void sayHello() {
-    System.out.println("Hello World!");
+    log.debug("Hello World!");
   }
 
   void notCovered() {
-    System.out.println("This method is not covered by unit tests");
+    log.debug("This method is not covered by unit tests");
   }
 
 }
