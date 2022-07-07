d6c0f2719ad6d519718d27186183f09a15735f72
bhanurekha09 <71223482+bhanurekha09@users.noreply.github.com>
2022-07-07 11:17:16 +0530

    Update HelloWorld.java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
 
-public class log4jExample{
+public class HelloWorld{
 
    /* Get actual class name to be printed on */
-   static Logger log = Logger.getLogger(log4jExample.class.getName());
+   static Logger log = Logger.getLogger(HelloWorld.class.getName());
    
    public static void main(String[] args)throws IOException,SQLException{
       log.debug("Hello this is a debug message");
